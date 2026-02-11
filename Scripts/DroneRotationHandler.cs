201370bf0a20674ffd59c67521607ac6b8bacfed
SkyValor <dragonboy_x@hotmail.com>
2026-02-11 01:31:19 -0100

    Implemented all necessary rotation. Missing naming consistency and fixing PlayerLookAtTarget be a separate object.

--- Scripts/DroneRotationHandler.cs
+++ Scripts/DroneRotationHandler.cs
@@ -25,6 +25,13 @@ public partial class DroneRotationHandler : Node
     public void SetYaw(float yaw) => this.yaw = yaw;
     public void SetThrottle(float throttle) => this.throttle = throttle;
 
+    // TODO: Node3D PlayerLookAtTarget must not be a child of PlayerDrone
+    // because when the PlayerDrone rotates in X-axis during pitch, it goes up or down
+    // and the PhantomCamera will rotate as well.
+    //
+    // Make the PlayerLookAtTarget separate from PlayerDrone, but react to its transformation,
+    // such as when it moves in World Space, and move according to it.
+    
     public void Tick(float delta)
     {
         UpdateRotationForce(ref currentPitch, inputPitch, PitchDelta);
@@ -32,8 +39,14 @@ public partial class DroneRotationHandler : Node
         
         UpdateRotationForce(ref currentRoll, inputRoll, RollDelta);
         currentRoll = Mathf.MoveToward(currentRoll, inputRoll, RollDelta * delta);
+
+        drone.RotationDegrees = drone.RotationDegrees with
+        {
+            // X = -currentPitch * PitchMaxDegrees,
+            Z = -currentRoll * RollMaxDegrees
+        };
         
-        drone.RotationDegrees += new Vector3(x: -currentPitch * PitchMaxDegrees, 0f, -currentRoll * RollMaxDegrees);
+        // drone.RotationDegrees += new Vector3(x: -currentPitch * PitchMaxDegrees, 0f, -currentRoll * RollMaxDegrees);
     }
 
     private static void UpdateRotationForce(ref float currentForce, float inputForce, float forceDelta)
