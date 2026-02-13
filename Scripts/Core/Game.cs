ac69f278bec3c90b500df2f0b50735a7951c2912
SkyValor <dragonboy_x@hotmail.com>
2026-02-13 03:27:32 -0100

    Started work on energy manager. Created a simple level to test all current features.

---
+++ Scripts/Core/Game.cs
@@ -0,0 +1,35 @@
+﻿namespace RescueDrone.Scripts.Core;
+
+using System.Collections.Generic;
+using Godot;
+using MEC;
+
+public partial class Game : Node
+{
+    [Export] private PackedScene PlayerScene { get; set; }
+    
+    
+    
+    [Export] private float timeBeforeStart { get; set; }
+
+    private Drone playerDrone;
+
+    private GameState gameState;
+    private CoroutineHandle? waitTimer;
+
+    public override void _Ready()
+    {
+        playerDrone = PlayerScene.Instantiate<Drone>();
+        AddChild(playerDrone);
+        var spawnPoint = GetNode<Node3D>("%PlayerSpawnPoint");
+        playerDrone.GlobalPosition = spawnPoint.GlobalPosition;
+        waitTimer = Timing.RunCoroutine(WaitAndStart());
+    }
+
+    private IEnumerator<double> WaitAndStart()
+    {
+        yield return Timing.WaitForSeconds(timeBeforeStart);
+        GD.Print("Game started!");
+    }
+    
+}
